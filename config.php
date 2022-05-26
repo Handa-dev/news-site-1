821e33fbb1bd6a5a4c5bee2388ab1826ac21932f
Handa-dev <68268588+Handa-dev@users.noreply.github.com>
2022-05-26 20:49:11 +1000

    Revert "Code For Dynamic Title"

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-$hostname = "http://localhost/news-site-1";
+$hostname = "http://localhost/news-site";
 $conn = mysqli_connect("localhost","root","","news-site") or die("connection failed : ". mysqli_connect_error());
 ?>
